eb67914ddc47d086e1a655604b2a3e832f998981
shuja990 <shujaali1234@gmail.com>
2020-08-26 14:13:23 +0500

    add

--- src/context.js
+++ src/context.js
@@ -24,7 +24,7 @@ class RoomProvider extends React.Component {
                 content_type: 'beachResortRoom',
                 order: 'sys.createdAt'
             })
-            let rooms = this.formatData(response.items)
+            let rooms = this.formatData(items)
             let featuredRooms = rooms.filter(room=>room.featured===true);
             let maxPrice = Math.max(...rooms.map(item=>item.price));
             let maxSize = Math.max(...rooms.map(item=>item.size));
